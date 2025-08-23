86e89e86ae7cf1d050cfd0701d3c64090f03acc3
Nagmani001 <nagmanipd3@gmail.com>
2025-08-23 15:12:37 +0545

    used prisma accelerate

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,11 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-// Extend the global object to include the PrismaClient instance
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = globalThis.prisma || new PrismaClient();
-// Ensure the PrismaClient instance is not recreated in development
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
-
-export default prisma;
+export const prisma = new PrismaClient().$extends(withAccelerate())
